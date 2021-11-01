f630b43341a869c223cfe3367d8ee86ecdc847d9
AlessandroGenerale17 <a.generale@student.vu.nl>
2021-11-01 23:46:47 +0100

    starting to understand sockets

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,37 @@
 import { useSelector } from 'react-redux';
 import Loading from '../../components/Loading';
 import { selectAppLoading } from '../../store/appState/selectors';
+import { io } from 'socket.io-client';
+import { useEffect } from 'react';
+
+const socket = io('http://localhost:4000');
 
 export default function Home() {
     const loading = useSelector(selectAppLoading);
 
+    // different domain
+    //const socket = io("https://server-domain.com");
+
+    useEffect(() => {
+        socket.on('private', (msg) => {
+            console.log(msg);
+        });
+        socket.on('second-channel', (msg) => {
+            console.log(msg);
+        });
+    }, []);
     if (loading) return <Loading />;
     return (
         <div>
             <h1>Home</h1>
+            <button
+                onClick={() => {
+                    socket.emit('message-server');
+                    socket.emit('message-secondChannel');
+                }}
+            >
+                Press
+            </button>
         </div>
     );
 }
